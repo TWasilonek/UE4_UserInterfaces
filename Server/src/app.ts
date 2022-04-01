9124ea179c7a4d1668542e19515f56b500eb6d99
TWasilonek <tomasz.wasilonek@gmail.com>
2022-04-01 19:50:24 +0200

    wired up authRouter

--- Server/src/app.ts
+++ Server/src/app.ts
@@ -1,12 +1,11 @@
 import express from 'express';
 import tasksRouter from './routes/tasksRoute';
-
+import authRouter from './routes/authRouter';
 
 const app = express();
 app.use(express.json());
 
 app.use('/api/tasks', tasksRouter);
-
-
+app.use('/api/auth', authRouter);
 
 export default app;
